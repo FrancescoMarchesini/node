9173749b4c57d7787bf3e7fb4b86092d497f16b8
FrancescoMarchesini <marchsinifrancesco1@gmail.com>
2019-11-23 17:52:58 +0100

    basic app configuration

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,16 +1,29 @@
-import _ from 'loadsh';
 import * as THREE from 'three';
 
-function component() {
-  const scene = new THREE.Scene();
-  var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-  
-  var renderer = new THREE.WebGLRenderer();
-  renderer.setSize( window.innerWidth, window.innerHeight );
+function main() {
+  const canvas = document.querySelector('#c');
+  const renderer = new THREE.WebGLRenderer({canvas});
 
-  
-  return renderer.domElement;
-  
-  }
-  
-  document.body.appendChild(component());
+  const fov = 75;
+  const aspect = 2;  // the canvas default
+  const near = 0.1;
+  const far = 5;
+  const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  camera.position.z = 2;
+
+  const scene = new THREE.Scene();
+
+  const boxWidth = 1;
+  const boxHeight = 1;
+  const boxDepth = 1;
+  const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+
+  const material = new THREE.MeshBasicMaterial({color: 0x44aa88});  // greenish blue
+
+  const cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+
+  renderer.render(scene, camera);
+}
+
+main();
